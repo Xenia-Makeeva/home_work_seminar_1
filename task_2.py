6712536a057119fbd19dee8d0f725bf0a26238a0
Denis Makeev <dvm77701@yandex.ru>
2022-09-11 20:33:52 +0300

    hw_1

---
+++ task_2.py
@@ -0,0 +1,11 @@
+#2. Задача: Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+def logical_statement(x, y, z):
+    print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z}')
+for x in range(0,2):
+    for y in range(0,2):
+        for z in range(0,2):
+            if ((not (x or y or z)) == (not x and not y and not z)):
+                print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} is True')
+            else:
+                print(f'¬({x} ⋁ {y} ⋁ {z}) = ¬{x} ⋀ ¬{y} ⋀ ¬{z} is False')
